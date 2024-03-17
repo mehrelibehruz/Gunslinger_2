117c5dfb021ccd99ed5a0c0cfb9eef158a9596ef
Bahruz Mehrali <mehrelibehruz@gmail.com>
2024-03-17 09:46:36 +0400

    Add project files.

---
+++ Assets/Scripts/Helper/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Helper
+{
+    public class Constants
+    {
+        public const string INPUT_HORIZONTAL = "Horizontal";
+        public const string INPUT_Jump = "Jump";
+        public const string TAG_DEATH_ZONE = "DeathZone";
+        public const string TAG_ENEMY = "Enemy";
+    }
+}
